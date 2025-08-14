759dc8d31ee804eaefc47411718e122a08c3319d
prodhanm <mustafizur.prodhan@gmail.com>
2025-08-14 18:01:06 -0400

    created the views and urls for the store app

--- store/views.py
+++ store/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from . models import Category, Product
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+def store(request):
+    all_products = Product.objects.all()
+    context = {'my_products': all_products}
+    return render(request, 'store/store.html', context=context)
+
+def categories(request):
+    all_categories = Category.objects.all()
+    return {'all_categories': all_categories}
+
+def list_category(request, category_slug=None):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    context = {'category': category, 'products': products}
+    return render(request, 'store/list_category.html', context=context)
+
+def product_info(request, product_slug):
+    product = get_object_or_404(Product, slug=product_slug)
+    context = {'product': product}
+    return render(request, 'store/product_info.html', context=context)
