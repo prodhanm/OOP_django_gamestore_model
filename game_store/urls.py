2a2428183d151cd990fa7655594592248a636aeb
prodhanm <mustafizur.prodhan@gmail.com>
2025-08-14 17:57:05 -0400

    set up the models for the store app and its admin interface

--- game_store/urls.py
+++ game_store/urls.py
@@ -23,10 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-    path('cart/', include('cart.urls')),
-    path('account/', include('account.urls')),
-    path('payment/', include('payment.urls')),
-    path('inventory/', include('inventory.urls')),
+    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . models import Product, Category
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+#admin.site.register(Category, CategoryAdmin)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'brand', 'slug', 'price', 'stock', 
+                    'available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('title',)}
+    list_per_page = 20
