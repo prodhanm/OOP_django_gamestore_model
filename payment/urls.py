e0fdb618e5beb35dc21fffecbe61841ea449e6d6
prodhanm <mustafizur.prodhan@gmail.com>
2025-08-14 20:29:53 -0400

    completed the setup for the payment app

--- payment/urls.py
+++ payment/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('payment_success/', views.payment_success, name='payment_success'),
+    path('payment_fail/', views.payment_fail, name='payment_fail'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('complete_order/', views.complete_order, name='complete_order'),
+]
