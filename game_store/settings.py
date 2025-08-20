8a1da18e8f5f86b30fd3e5567440235ded6e95cc
prodhanm <mustafizur.prodhan@gmail.com>
2025-08-20 13:38:10 -0400

    modified settings

--- game_store/settings.py
+++ game_store/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+#CSRF_TRUSTED_ORIGINS = ['']
 
 
 # Application definition
