da31792d627d637bc97da2b6280b18039ffd3617
prodhanm <mustafizur.prodhan@gmail.com>
2025-08-22 23:55:55 -0400

    modified settings

--- game_store/settings.py
+++ game_store/settings.py
@@ -193,6 +193,7 @@ DATABASES = {
         'USER': os.environ.get('USER'),
         'PASSWORD': os.environ.get('PASSWORD'),
         'HOST': os.environ.get('HOST'),
-        'PORT': os.environ.get('PORT'),
+        #'PORT': os.environ.get('PORT'),
+        'PORT': '5432',
     }
 }
